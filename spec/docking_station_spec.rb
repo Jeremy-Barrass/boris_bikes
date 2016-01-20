7310e0384e9c3aa8da9c6bd5e8e987be67b8d439
yiorgoss <makridakisyiorgos@gmail.com>
2016-01-20 10:14:57 +0000

    DAY ONE FINAL COMMIT

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,9 +3,18 @@ require "docking_station"
 
 describe DockingStation do
   it {is_expected.to respond_to :release_bike}
+  it {is_expected.to respond_to(:dock).with(1).argument}
 
   it "expects station to release bikes" do
-    bike = subject.release_bike
-    expect(bike).to be_working
+    instance = subject.release_bike
+    expect(instance).to be_working
+
   end
+  it "expects to see if bike is docked" do
+    instance = subject.bike
+    expect(instance).to eq(@bike)
+
+  end
+
+
 end
