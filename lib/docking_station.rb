81602a473f50daeec4fef4749bc2076ec9bf7fd9
Jeremy Barrass <jeremybarrass@Jeremys-MacBook-Pro.local>
2016-01-20 15:17:13 +0000

    Challenge 13: Docking Capacity complete - raise error if bike already docked added.

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -4,16 +4,13 @@ class DockingStation
 
   attr_reader :bike
 
-  def initialize
-
-  end
-
   def release_bike
     raise "No bikes are available." unless @bike
     @bike
   end
 
   def dock(bike)
+    raise "A bike is already docked." unless !@bike
     @bike = bike
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,26 +1,33 @@
 # In spec/docking_station_spec.rb
 require "docking_station"
 
-  describe DockingStation do
-    it {is_expected.to respond_to :release_bike}
-    it {is_expected.to respond_to(:dock).with(1).argument}
+describe DockingStation do
+  it {is_expected.to respond_to :release_bike}
+  it {is_expected.to respond_to(:dock).with(1).argument}
 
-    describe "#release_bike" do
-      it "expects station to release bikes" do
-        bike = Bike.new
-        subject.dock bike
-        instance = subject.release_bike
-        expect(instance).to be_instance_of Bike
-      end
-
-      it "Raises an error if no bikes are available." do
-
-        expect{subject.release_bike}.to raise_error "No bikes are available."
-      end
+  describe "#release_bike" do
+    it "expects station to release bikes" do
+    bike = Bike.new
+      subject.dock bike
+      instance = subject.release_bike
+      expect(instance).to be_instance_of Bike
     end
 
+    it "Raises an error if no bikes are available." do
+      expect{subject.release_bike}.to raise_error "No bikes are available."
+    end
+  end
+
+  describe "#dock" do
     it "expects to see if bike is docked" do
       instance = subject.bike
       expect(instance).to eq(@bike)
     end
+
+    it "Raises an error if a bike is already docked." do
+      bike = Bike.new
+      subject.dock bike
+      expect{subject.dock(bike)}.to raise_error "A bike is already docked."
+    end
   end
+end
